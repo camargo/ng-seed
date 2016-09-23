f4db77a6d201c772da383ab0954450ae1c5ee678
Chris Camargo <Christopher.A.Camargo@jpl.nasa.gov>
2016-09-23 09:09:34 -0700

    fix unit test

--- src/app/shared/services/app-api/app-api.service.spec.ts
+++ src/app/shared/services/app-api/app-api.service.spec.ts
@@ -1,10 +1,14 @@
 import { inject, TestBed } from '@angular/core/testing';
+import { HttpModule } from '@angular/http';
 
 import { AppApiService } from './app-api.service';
 
 describe('App Api Service', () => {
   beforeEach(() => {
-    TestBed.configureTestingModule({providers: [AppApiService]});
+    TestBed.configureTestingModule({
+      imports: [HttpModule],
+      providers: [AppApiService]
+    });
   });
 
   it('should ...', inject([AppApiService], (appApiService: AppApiService) => {
